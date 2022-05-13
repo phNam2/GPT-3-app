769010559d3475cc2051859d4b13cff2a85628bb
nam <namhoangpham96@gmail.com>
2022-05-13 13:39:20 -0400

    Finish the basic requirement

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 async function enter () {
     const prompt = document.getElementById("prompt").value;
     document.getElementById("prompt").value = "";
@@ -13,6 +11,35 @@ async function enter () {
         body: JSON.stringify(data)
     };
     const response = await fetch('/completions', options);
-    const json = await response.json();
-    console.log(json);
+    const result = await response.json();
+    console.log(result);
+
+    // Create the response part
+    const res = document.getElementById("response");
+    let group = document.createElement('i');
+    let prompting = document.createElement('div');
+    let prompTitle = document.createElement('h3');
+    let promtDescription = document.createElement('p');
+    let responding = document.createElement('div');
+    let responseTitle = document.createElement('h3');
+    let responseDes = document.createElement('p');
+
+    prompTitle.innerHTML = "Prompt:";
+    promtDescription.innerHTML = result.prompt;
+    prompting.appendChild(prompTitle);
+    prompting.appendChild(promtDescription);
+    responseTitle.innerHTML = "Response: ";
+    responseDes.innerHTML = result.output;
+    responding.appendChild(responseTitle);
+    responding.appendChild(responseDes);
+
+    group.appendChild(prompting);
+    group.appendChild(responding);
+
+    if (res.firstChild == null) {
+        res.appendChild(group);
+    } else {
+        res.insertBefore(group, res.firstChild);
+    }
+    
 }
