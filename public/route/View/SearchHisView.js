6b8eed4d116fe81434a2a056567cd28f905a146c
nam <namhoangpham96@gmail.com>
2022-05-16 15:02:22 -0400

    Current design

--- public/route/View/SearchHisView.js
+++ public/route/View/SearchHisView.js
@@ -9,20 +9,12 @@ export default class extends AbstracView {
     async getHtml() {
         return `
             <h1>Search History:</h1>
+            <button id="historyButton" onclick="addHistory()">
+                <span class="text">Show</span>
+            </button>
 
             <div id="historyContainer">
-                <div id="000000000">
-                    <div class="collapsible">Open Collapsible</div>
-                    <div class="content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                </div>
-                <div id="000000001">
-                    <div class="collapsible">Open Collapsible</div>
-                    <div class="content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                </div>
+
             </div>`;
     }
 }
